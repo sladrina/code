d073eb89dce5b32b07c4c9dab66ea0cfcc26b41c
Arina Rybak <sladrina@gmail.com>
2023-02-24 18:27:55 +0400

    Updated basket

--- basket/script.js
+++ basket/script.js
@@ -1,35 +1,50 @@
 let items = document.getElementsByClassName('item'),
-    recycle = document.getElementById('recycle'),
-    infotext = document.getElementById('infotext'),
+    recycle = document.getElementById('basket'),
+    infotext = document.getElementById('info_text'),
     summ = document.getElementsByClassName('summ'),
-    del_button = document.getElementsByClassName('del_button')
+    del_button = document.getElementsByClassName('del_button');
+    prices = recycle.getElementsByClassName('price');
+    buttonClear = document.getElementsByClassName('clear')[0];
+    total = 'Total price: '
+
 function addItem() {
     infotext.style.display = 'none';
     let item_clone = this.parentElement.cloneNode(true);
     recycle.appendChild(item_clone)
 
-    let prices = recycle.getElementsByClassName('price')
-    totalprice = 0;
+    let totalprice = 0;
+
+    buttonClear.classList.remove("hidden")
     for (let j = 0; j < prices.length; j++) {
         totalprice = totalprice + Number(prices[j].innerHTML);
     }
-    console.log('Общая стоимость', totalprice);
-    summ[0].innerHTML = 'Общая стоимость ' + totalprice;
+    summ[0].innerHTML = total + totalprice;
 }
 let item_button = document.getElementsByClassName('item_button');
-for (var i = 0; i < item_button.length; i++) {
+for (let i = 0; i < item_button.length; i++) {
     item_button[i].onclick = addItem;
-};
-let buttonClear = document.getElementById('clear');
-buttonClear.onclick = function() {
-    recycle.innerHTML = ''
-    recycle.appendChild(infotext)
-    infotext.style.display = 'block';
+}
+
+function deleteInfo() {
+    recycle.appendChild(infotext);
+    infotext.style.display = 'flex';
+    buttonClear.classList.add("hidden")
     summ[0].innerText = '';
 }
 
+buttonClear.onclick = function() {
+    recycle.innerHTML = '';
+    deleteInfo();
+}
+
 function delItem() {
-    this.parentElement.remove()
+    this.parentElement.remove();
+    let price = parseInt(summ[0].innerText.match(/\d+/)) - this.parentElement.getElementsByClassName('price')[0].innerHTML;
+    summ[0].innerText = total + price;
+
+    if (prices.length <1) {
+        deleteInfo();
+    }
 }
 
 for (var i = 0; i < del_button.length; i++) {
